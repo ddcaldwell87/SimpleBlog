da664558a4303bbea745d411d94cc71dcd2e58b9
David-PC\David <ddcaldwell87@gmail.com>
2016-11-19 19:35:56 -0500

    Implemented form authentication and custom role provider

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "admin")] // Allows access to this controller only if  user role is admin.
     public class UsersController : Controller
     {
         public ActionResult Index()
--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SimpleBlog.ViewModels;
+using System.Web.Security; // Used by FormsAuthentication.
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToRoute("home");
+        }
+
         public ActionResult Login()
         {
             return View(new AuthLogin
@@ -17,18 +24,17 @@ namespace SimpleBlog.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(AuthLogin form)
+        public ActionResult Login(AuthLogin form, string returnUrl)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) // Returns the view if the form is invalid.
                 return View(form);
 
-            if (form.Username != "rainbow dash")
-            {
-                ModelState.AddModelError("Username", "Username or password are not 20% cooler.");
-                return View(form);
-            }
+            FormsAuthentication.SetAuthCookie(form.Username, true); // Gives username a authentication cookie.
 
-            return Content("This form is valid!");
+            if (!string.IsNullOrWhiteSpace(returnUrl))
+                return Redirect(returnUrl);
+
+            return RedirectToRoute("home");
         }
     }
 }
