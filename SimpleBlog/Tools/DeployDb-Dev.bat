7e8bb989efd9631470aea7834e362e2e3bd40239
David-PC\David <ddcaldwell87@gmail.com>
2016-11-21 18:52:24 -0500

    Added mySQL driver

--- SimpleBlog/Tools/DeployDb-Dev.bat
+++ SimpleBlog/Tools/DeployDb-Dev.bat
@@ -1,1 +1,1 @@
- ..\..\packages\FluentMigrator.1.6.2\tools\Migrate.exe --db=mysql --target=..\bin\simpleblog.dll --configPath=..\Web.config -c=MainDb -t=rollback
+ ..\..\packages\FluentMigrator.1.6.2\tools\Migrate.exe --db=mysql --target=..\bin\simpleblog.dll --configPath=..\Web.config -c=MainDb
